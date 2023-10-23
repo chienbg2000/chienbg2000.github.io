4c7cb9cc606e002258c366f456cf94bd0bdf1506
chienbg200400 <chienbg200400@DESKTOP-GMJM31I>
2023-10-24 00:29:01 +0700

    update

--- src/components/Contact.vue
+++
@@ -1,9 +0,0 @@
-<template>
-   <h1>OKE</h1>
-</template>
-
-<script>
-export default {
-   name: 'ContactPage'
-}
-</script>
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiUrl: 'https://script.google.com/macros/s/AKfycbxmYK947WpbboAbG0EB_KyXQk0Ix-TMmMcA_dBQ57za8_zBmkn3_eECtLUwIW0SLpRbwg/exec',
-  };
-  
